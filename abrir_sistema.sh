a35b2f8480f01ced7299bd73dbbfd50ec289b188
Diogo Moreira <diogopmoreirap@gmail.com>
2026-03-27 22:41:22 +0000

    script

--- abrir_sistema.sh
+++ abrir_sistema.sh
@@ -3,8 +3,8 @@
 # 1. Entra na pasta do projeto
 cd "/home/teu_utilizador/pasta_do_projeto"
 
-# 2. Abre a Cozinha em segundo plano usando o Python do venv
-./venv/bin/python cozinha.py &
-
 # 3. Abre a Caixa usando o Python do venv
 ./venv/bin/python caixa.py
+
+# 2. Abre a Cozinha em segundo plano usando o Python do venv
+./venv/bin/python cozinha.py &
